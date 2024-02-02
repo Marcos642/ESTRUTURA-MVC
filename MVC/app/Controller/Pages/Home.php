3ecf1ce74342332af4de6795d5c1a763f0d33c9c
Marcos Tadeu de Brito Torres <94060220+Marcos642@users.noreply.github.com>
2024-02-02 11:11:27 -0300

    Update Home.php

--- MVC/app/Controller/Pages/Home.php
+++ MVC/app/Controller/Pages/Home.php
@@ -1,11 +1,12 @@
 <?php
-   //Responsavel por gerenciar as requisições da homer do site (Pagina inicial)
 namespace App\Controller\Pages;
-
 use \App\utils\view;  // para usar a classe
+
 class Home{
     /*
-     * Metodos responsavel por retornar o conteudo (View) da Homer
+    Este método utiliza a classe view para renderizar a view chamada 'pages\home', passando um array
+    associativo de variáveis (name e description). Os valores dessas variáveis serão substituídos na view,
+    resultando em uma página HTML personalizada.
     */
     public static function getHome(){
         return view::render('pages\home',[
@@ -14,6 +15,3 @@ class Home{
         ]);
     }
 }
-
-// Só que o controler é responsavel por pegar os dados que o model retornou
-// e colocalos dentro da view
